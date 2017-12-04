cef8eaa6638efc1d96d87bb2fd99bab24eb87ae6
TheoRev <hrevillafernandez79@gmail.com>
2017-12-04 01:11:35 -0500

    Creacion de crud de patient

--- public/js/controller/patient/patientController.js
+++ public/js/controller/patient/patientController.js
@@ -3,24 +3,45 @@ $(document).ready(function() {
   var url_1 = "http://192.168.1.33/odontosoft-php/api/patient_api.php";
   var url_2 = "http://localhost/odontosoft-php/api/patient_api.php";
   var url_3 = "http://localhost:3030/api/patients/";
+
+  // var patients = null;
+
   $(function() {
-    $.ajax({
-      type: "GET",
-      url: url_2,
-      data: {
-        opt: "all"
+    $("#tblPatients").puidatatable({
+      caption: "Lista de Pacientes",
+      paginator: {
+        rows: 10
       },
-      dataType: "json",
-      success: success,
-      error: error
+      responsive: false,
+      columns: [
+        { field: "id", headerText: "ID" },
+        { field: "date_init", headerText: "Fec. Ingr.", pattern: 'dd/MM/yyyy' },
+        { field: "nom_ape", headerText: "Nombres y Ape.", filter: true },
+        { field: "age", headerText: "Edad" },
+        { field: "sex", headerText: "Sexo" },
+        { field: "date_nac", headerText: "Fec. Nac." },
+        { field: "diabettes", headerText: "Diabts." },
+        { field: "hipertension", headerText: "Hiper." }
+      ],
+      datasource: function(callback) {
+        $.ajax({
+          type: "GET",
+          url: "http://localhost/odontosoft-php/api/patient_api.php",
+          data: {
+            opt: "all"
+          },
+          dataType: "json",
+          context: this,
+          success: function(response) {
+            let patients;
+            for (let i = 0; i < response.length; i++) {
+              // console.log(response[i]);
+
+            }
+            callback.call(this, response);
+          }
+        });
+      }
     });
-
-    function success(response) {
-      console.log(response);
-    }
-
-    function error(response) {
-      console.log(response);
-    }
   });
 });
